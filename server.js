b6d3237183b95177136f73f23b4a9b68c5e1b74b
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-08 19:55:02 -0400

    server set up

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socially-awkward', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
