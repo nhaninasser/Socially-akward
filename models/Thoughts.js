17624c49b2d7fb7b08e63c2caad4837eda48577f
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-09 18:56:05 -0400

    added date formatting

---
+++ models/Thoughts.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reactions");
+const dateFormat = require("../utils/dateFormat");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtBody: {
+      type: String,
+      required: "Please leave a thought!",
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
