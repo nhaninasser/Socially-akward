f6c130fd102e1612f19bc14b3777e44671ef6249
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-09 19:37:09 -0400

    thought controller setup

---
+++ controllers/thought-controller.js
@@ -0,0 +1,124 @@
+const { Thought, User } = require("../models");
+
+
+const thoughtController = {
+  getAllThoghts(req, res) {
+    Thought.find({})
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+
+
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.thoughtId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "THOUGHT ID NOT FOUND!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+
+
+  addThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        console.log(_id);
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        console.log(dbUserData);
+        if (!dbUserData) {
+          res.status(404).json({ message: "USER ID NOT FOUND!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+
+  updateThought({ params, body }, res) {
+    console.log(params.thoughtId);
+    console.log(body);
+    Thought.findOneAndUpdate({ _id: params.thoughtId }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "THOUGHT ID NOT FOUND!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+
+  removeThough({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then((deletedThought) => {
+        if (!deletedThought) {
+           res.status(404).json({ message: "THOUGHT ID NOT FOUND!" });
+           return;
+        }
+        console.log(deletedThought);
+        User.findOneAndUpdate(
+          { username: deletedThought.username },
+          { $pull: { thoughts: params.thoughtId } },
+          { new: true }
+        ).then((dbUserData) => {
+          if (!dbUserData) {
+            res.status(404).json({ message: "USER ID NOT FOUND!" });
+            return;
+          }
+          res.json(dbUserData);
+        });
+      })
+      .catch((err) => res.json(err));
+  },
+
+
+  addReaction({ params, body }, res) {
+    Thought.findOnAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "USER ID NOT FOUND!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdatE(
+      { _id: params.thoughtId },
+      { $pull: { reaction: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+};
+
+
+
+module.exports = thoughtController;
